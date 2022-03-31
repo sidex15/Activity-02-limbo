a677b0f18bd7ffddeed99fdd4e61891ca2d81778
sidex15 <moses.limbo@gmail.com>
2022-03-31 13:58:33 +0800

    Added Comments

--- Pokemon-Damage-Calculator.py
+++ Pokemon-Damage-Calculator.py
@@ -1,6 +1,15 @@
 import math
 import random
 
+#Pokemon Damage Calculator
+
+#A Lv. 90 Charizard (Fire/Flying, Sp. Atk: 205) attacks a Lv. 95
+#Feraligatr (Water, Sp. Def: 188) with Fire Blast, a Fire-type move
+#with a Power of 110 and gains a same-type attack bonus.. It hits
+#the target normally but deals a not very effective damage. The
+#weather on the field is normal. Given the following conditions,
+#determine how much damage Charizard’s attack will deal.
+
 pokemon = {
     "lvl":90,
     "atk":205,
